1ace2971d032684491807b36e0f01ab50e98d811
empireelango777@gmail.com <DELL@elangovan.dteagritech.com>
2024-10-01 14:17:49 +0530

    learn : update

---
+++ basics/example.js
@@ -0,0 +1,11 @@
+
+
+var namex="elango"
+var age=22;
+
+function displayuser(username,userAge){
+    var aa ="elango"
+    return "username is : " + username + " " + "userage : "+userAge;
+}
+const detail=displayuser(namex,age)
+console.log(detail);
